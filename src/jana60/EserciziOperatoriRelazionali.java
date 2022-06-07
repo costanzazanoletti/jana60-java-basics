1db31c969e9b6cea8265ddb76d78ee38ee4a92e4
costanzazanoletti <zanoletti.cos@gmail.com>
2022-06-07 12:52:19 +0200

    aggiunti esercizi operatori relazionali

---
+++ src/jana60/EserciziOperatoriRelazionali.java
@@ -0,0 +1,32 @@
+package jana60;
+
+public class EserciziOperatoriRelazionali {
+  public static void main(String[] args) {
+
+    int x = 7;
+    int y = 5;
+
+    if (x == y) {
+      System.out.println("x uguale a y");
+      // altro codice
+    } else if (x < y) {
+      System.out.println("x minore di y");
+    } else {
+      System.out.println("x maggiore di y");
+    }
+
+    System.out.println("arrivederci");
+
+
+    if (!(x == y)) {
+      System.out.println("x non è uguale a y");
+    }
+
+    if (x != y && x > 10) {
+      // entro nell'if
+    }
+
+
+  }
+
+}
